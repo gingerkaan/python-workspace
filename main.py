18df9b67cdda6b66399e8a92c8ac1038c8134e1d
Kaan Girginer <kaangirginer@icloud.com>
2022-01-02 02:03:10 +0300

    test commit vscode

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 print("hello world!")
 print("hello world!")
 print("2nd commit for before working")
+
+ex_8 = "orange"
+print(ex_8[2])
+print("apple"[4])
+
+ex_10 = "apricots"
+## string slicing
+print(ex_10[:3])
+print(ex_10[2:5])
+print(ex_10[4:])
+r2d2 = "R2" + "-" + "D2"
+print(r2d2)
+
+##type()
+ex_6 = 3.21 
+print(type(ex_6))
+new_ex_6 = str(ex_6)
+print(type(new_ex_6))
